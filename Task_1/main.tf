7d49d94a23f7f3eb622d8d4543326fac61c08ecb
Stanislav Mestechkin <s.mestechkin@gmail.com>
2026-02-09 22:49:37 +0000

    Fix Task 1 solution

--- Task_1/main.tf
+++ Task_1/main.tf
@@ -39,3 +39,42 @@ resource "aws_internet_gateway" "exam_ig" {
     "Created" = "terraform"
   }
 }
+
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.exam_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.exam_ig.id
+  }
+
+  tags = {
+    "Name"    = "Private Route Table"
+    "Created" = "Terraform"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.exam_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.exam_ig.id
+  }
+
+  tags = {
+    "Name"    = "Private Route Table"
+    "Created" = "Terraform"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
