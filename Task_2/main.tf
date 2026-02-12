36ceb72421a8fc7b17afce2e8be4b7298a5f0fb2
Stanislav Mestechkin <s.mestechkin@gmail.com>
2026-02-12 23:11:58 +0000

    Make private route table private

--- Task_2/main.tf
+++ Task_2/main.tf
@@ -53,11 +53,6 @@ resource "aws_route_table" "public_rt" {
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.exam_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.exam_ig.id
-  }
-
   tags = {
     "Name"    = "Private Route Table"
     "Created" = "Terraform"
